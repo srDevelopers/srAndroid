99ce9a0e70389d23d7a1abb2f413b3b342252c4c
Junseok Oh <kotohana5706@edcan.kr>
2016-11-03 15:28:30 +0900

    Implement SrNowActivity

--- app/src/main/java/kr/edcan/srandroid/MainActivity.kt
+++ app/src/main/java/kr/edcan/srandroid/MainActivity.kt
@@ -2,11 +2,20 @@ package kr.edcan.srandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        setLayout()
+    }
+
+    private fun setLayout() {
+        openSrNow.setOnClickListener { startActivity<SrNowActivity>() }
     }
 }
---
+++ app/src/main/java/kr/edcan/srandroid/SrNowActivity.kt
@@ -0,0 +1,12 @@
+package kr.edcan.srandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SrNowActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sr_now)
+    }
+}
